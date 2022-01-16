6f02e0220e5686e6c803e291151c94370e236eac
hailinh2002 <97841360+hailinh2002@users.noreply.github.com>
2022-01-16 22:19:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -16,5 +16,4 @@ const CONFIG = {
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://m.me/hailinh2k2' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
+    messLink: 'https://m.me/hailinh2k2'
